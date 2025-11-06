36c7db0ff575be1f47e7dfbe0d9491abb0e041fd
BlackWater Park <blairdeleon@protonmail.com>
2025-11-06 20:10:09 +0100

    add  files and ctr

--- MATPLOTLIB/main.py
+++ MATPLOTLIB/main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+sns.set_theme()
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
@@ -14,7 +14,6 @@ ax.set_title('Square Numbers', fontsize=30)
 ax.set_xlabel('Value', fontsize=15)
 ax.set_ylabel('Square of Value', fontsize=15)
 ax.tick_params(axis='both', labelsize=12)
-ax.grid()
+ax.grid(True) # или так можно ax.grid() просто добавляет сетку
 ax.legend()
-fig.tight_layout()
 plt.show()
